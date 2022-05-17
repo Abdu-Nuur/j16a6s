fa1507b2b1b79c7425f91ee0c406cb08c2326813
abdunuur <abdunuura@gmail.com>
2022-05-17 11:45:56 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ведите радиус круга");
+        int radius=scanner.nextInt();
+
+        Circle circle = new Circle(3.14159);
+
+        Circle.area(circle.getPI(),radius);
+        Circle.circumference(circle.getPI(),radius);
+    }
+}
